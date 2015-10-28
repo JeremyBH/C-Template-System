fe8db3e3bc4cd616fab81279b89dc95f57b1db49
Benjamin Kohler <bkohler616@hotmail.com>
2015-10-28 14:39:28 -0400

    Setup Solution. Added some basic programmatic objects to the form.

---
+++ TemplateSystem/TemplateSystem/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TemplateSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() {
+            Button hello = new Button {
+                Content = "Hello",
+                Width = 20,
+                Height = 40
+            };
+                InitializeComponent();
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength() });
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength() });
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength() });
+            MainGrid.Children.Add(hello);
+            Grid.SetColumn(hello,2);
+            
+        }
+    }
+}
