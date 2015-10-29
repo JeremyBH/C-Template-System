622d9aef1fad389261993ad1967a82052af9ec96
Benjamin Kohler <bkohler616@hotmail.com>
2015-10-29 12:47:12 -0400

    Created main design windows required to add content to the templates.
    ModifyItem [WPF Window]:
    -Will be used to add items to the template, or modify the item.
    -Double clicking an item to modify, context menu add to add an item.
    
    Templater [WPF Window]:
    -The main visual designer for the template. Utilizing ModifyItem we can add different sizes and other things.
    -We will most likely want to keep the style in a grid format, so we can let the user add more grid colunms and rows if needed.

--- TemplateSystem/TemplateSystem/ModifyItem.xaml.cs
+++ TemplateSystem/TemplateSystem/ModifyItem.xaml.cs
@@ -10,23 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TemplateSystem
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ModifyItem.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ModifyItem : Window
     {
-        public MainWindow() {
-            
-        }
-
-        private void NewTemplate_OnClick(object sender, RoutedEventArgs e) {
-            Templater newForm = new Templater();
-            newForm.Show();
+        public ModifyItem()
+        {
+            InitializeComponent();
         }
     }
 }
--- TemplateSystem/TemplateSystem/Templater.xaml.cs
+++ TemplateSystem/TemplateSystem/Templater.xaml.cs
@@ -10,23 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TemplateSystem
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Templater.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Templater : Window
     {
-        public MainWindow() {
-            
+        public Templater()
+        {
+            InitializeComponent();
         }
 
-        private void NewTemplate_OnClick(object sender, RoutedEventArgs e) {
-            Templater newForm = new Templater();
-            newForm.Show();
+        private void ContextMenu_AddItem_OnClick(object sender, RoutedEventArgs e) {
+            ModifyItem newItem = new ModifyItem();
+            newItem.ShowDialog();
         }
     }
 }
